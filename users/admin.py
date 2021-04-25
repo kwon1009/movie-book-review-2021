07e6f78dba82ce5f8cbbb78255898b2f2cb357e6
kwonhs <47519135+kwon1009@users.noreply.github.com>
2021-04-26 02:27:33 +0900

    users and cores model
    - setting apps
    - users model and admin
    - cores model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin Definition """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "More",
+            {
+                "fields": (
+                    "avatar",
+                    "login_method",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "username",
+        "email",
+        "avatar",
+        "login_method",
+        "is_staff",
+    )
+
+    list_filter = ("login_method",)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """ Custom User Model Definition """
+
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGING_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGING_KAKAO, "Kakao"),
+    )
+
+    avatar = models.ImageField(blank=True, null=True)
+    nickname = models.CharField(max_length=10, null=True)
+    about_me = models.TextField(blank=True, default="")
+    login_method = models.CharField(
+        max_length=10, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
+
+    # my_list
+    # like_list
+    # wish_list
